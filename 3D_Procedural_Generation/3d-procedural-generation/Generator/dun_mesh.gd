199f47d14b4d13d8ddc7b8afacbdc261994beaa1
Jerek Wilson <anomalyaces@gmail.com>
2024-09-26 07:42:43 -0400

    completed tutorial. Opted to not care about corners

--- 3D_Procedural_Generation/3d-procedural-generation/Generator/dun_mesh.gd
+++ 3D_Procedural_Generation/3d-procedural-generation/Generator/dun_mesh.gd
@@ -28,7 +28,7 @@ func _ready() -> void:
 
 
 #Setters
-func set_start(val:bool)->void:
+func set_start(_val:bool)->void:
 	if Engine.is_editor_hint():
 		create_dungeon()
 
@@ -38,8 +38,6 @@ func set_start(val:bool)->void:
 #Handler Functions
 func handle_none(cell:Node3D, dir: String ):
 	cell.call("remove_door_"+dir)
-	#TODO - remove the corners as well
-
 
 # This naming convention uses the tile indexes of the types of tiles we care about
 # i.e. handle_00 is going from a room tile (idx:0) to another room tile (idx:0)
@@ -48,35 +46,147 @@ func handle_none(cell:Node3D, dir: String ):
 func handle_00(cell:Node3D, dir: String ):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
+
 #Room to Hallway
 func handle_01(cell:Node3D, dir: String ):
 	cell.call("remove_door_"+dir)
+
 #Room to Door
 func handle_02(cell:Node3D, dir: String ):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
+
 #Hallway to Room
 func handle_10(cell:Node3D, dir: String ):
 	cell.call("remove_door_"+dir)
+
 #Hallway to Hallway
 func handle_11(cell:Node3D, dir: String ):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
+
+
 #Hallway to Door
 func handle_12(cell:Node3D, dir: String ):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
+
+
 #Door to Room
 func handle_20(cell:Node3D, dir: String ):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
+
 #Door to Hallway
 func handle_21(cell:Node3D, dir: String ):
 	cell.call("remove_wall_"+dir)
+
 #Door to Door
 func handle_22(cell:Node3D, dir: String ):
 	cell.call("remove_wall_"+dir)
 	cell.call("remove_door_"+dir)
+
+
+#func handle_corners(cell: Node3D ):
+	#var idx = grid_map.get_cell_item(cell.position)
+	#
+	#var north_idx = grid_map.get_cell_item(cell.position + Vector3.FORWARD)
+	#var south_idx = grid_map.get_cell_item(cell.position + Vector3.BACK)
+	#var west_idx = grid_map.get_cell_item(cell.position + Vector3.LEFT)
+	#var east_idx = grid_map.get_cell_item(cell.position + Vector3.RIGHT)
+	#var nw_idx = grid_map.get_cell_item(cell.position + Vector3.FORWARD + Vector3.RIGHT)
+	#var ne_idx = grid_map.get_cell_item(cell.position + Vector3.FORWARD + Vector3.LEFT)
+	#var sw_idx = grid_map.get_cell_item(cell.position + Vector3.BACK + Vector3.LEFT)
+	#var se_idx = grid_map.get_cell_item(cell.position + Vector3.BACK + Vector3.RIGHT)
+	#
+	#
+	#var corners: Array[String] = []
+	#
+	#if _should_make_corner(idx, north_idx) && _should_make_corner(idx, west_idx):
+		#corners.append("nw")
+	#if _should_make_corner(idx, north_idx) && _should_make_corner(idx, east_idx):
+		#corners.append("ne")
+	#if _should_make_corner(idx, south_idx) && _should_make_corner(idx, west_idx):
+		#corners.append("sw")
+	#if _should_make_corner(idx, south_idx) && _should_make_corner(idx, east_idx):
+		#corners.append("se")
+	#
+	##Handle Hallway inside corners
+	#if idx == HALLWAY_TILE:
+		##Corners with 3 hallway tiles
+		#if north_idx == HALLWAY_TILE && west_idx == HALLWAY_TILE && _is_empty_tile(nw_idx) && nw_idx != DOOR_TILE:
+			#corners.append("nw_inv")
+		#if north_idx == HALLWAY_TILE && east_idx == HALLWAY_TILE && _is_empty_tile(ne_idx) && ne_idx != DOOR_TILE:
+			#corners.append("ne_inv")
+		#if south_idx == HALLWAY_TILE && west_idx == HALLWAY_TILE && _is_empty_tile(sw_idx) && sw_idx != DOOR_TILE:
+			#corners.append("sw_inv")
+		#if south_idx == HALLWAY_TILE && east_idx == HALLWAY_TILE && _is_empty_tile(se_idx) && se_idx != DOOR_TILE:
+			#corners.append("se_inv")
+		#
+		##Corners with 2 Hallway tiles
+		#if north_idx == HALLWAY_TILE \
+			#&& _is_empty_tile(west_idx) \
+			#&& _is_empty_tile(east_idx) \
+			#&& _is_empty_tile(south_idx) \
+			#&& _is_empty_tile(nw_idx) \
+			#&& _is_empty_tile(ne_idx) \
+			#&& _is_empty_tile(sw_idx) \
+			#&& _is_empty_tile(se_idx):
+			#corners.append_array(["sw", "se"])
+		#
+		#if south_idx == HALLWAY_TILE \
+			#&& _is_empty_tile(west_idx) \
+			#&& _is_empty_tile(east_idx) \
+			#&& _is_empty_tile(north_idx) \
+			#&& _is_empty_tile(nw_idx) \
+			#&& _is_empty_tile(ne_idx) \
+			#&& _is_empty_tile(sw_idx) \
+			#&& _is_empty_tile(se_idx):
+			#corners.append_array(["nw", "ne"])
+		#
+		#if east_idx == HALLWAY_TILE \
+			#&& _is_empty_tile(west_idx) \
+			#&& _is_empty_tile(south_idx) \
+			#&& _is_empty_tile(north_idx) \
+			#&& _is_empty_tile(nw_idx) \
+			#&& _is_empty_tile(ne_idx) \
+			#&& _is_empty_tile(sw_idx) \
+			#&& _is_empty_tile(se_idx):
+			#corners.append_array(["nw", "sw"])
+		#
+		#if west_idx == HALLWAY_TILE \
+			#&& _is_empty_tile(east_idx) \
+			#&& _is_empty_tile(south_idx) \
+			#&& _is_empty_tile(north_idx) \
+			#&& _is_empty_tile(nw_idx) \
+			#&& _is_empty_tile(ne_idx) \
+			#&& _is_empty_tile(sw_idx) \
+			#&& _is_empty_tile(se_idx):
+			#corners.append_array(["ne", "se"])
+		#
+		#
+	#
+	#
+	#cell.call("remove_corners", corners)
+	#
+	#
+	#
+#
+#func _should_make_corner(cur_idx: int, idx: int) -> bool:
+	## True Conditions
+	## 1. neighbor idx is Empty or neighbor idx is a border tile
+	## 2. curr idx != idx && curr idx is not a room tile and idx is not a door tile
+	#if _is_empty_tile(idx):
+		#return true
+	#if cur_idx != idx && (idx != DOOR_TILE && cur_idx != DOOR_TILE): 
+		#return true
+	##if cur_idx == idx && (cur_idx == HALLWAY_TILE && idx == HALLWAY_TILE):
+		##return false
+	#
+	#return false
+#
+#func _is_empty_tile(idx: int):
+	#return idx == grid_map.INVALID_CELL_ITEM || idx == BORDER_TILE
 
 func create_dungeon():
 	if(grid_map == null):
@@ -90,7 +200,7 @@ func create_dungeon():
 		var cell_index : int = grid_map.get_cell_item(cell)
 		if cell_index <= DOOR_TILE && cell_index >= ROOM_TILE:
 			var dun_cell: Node3D = dun_cell_scene.instantiate()
-			dun_cell.position = Vector3(cell) + Vector3(0.5, 0, 0.5) #This offest should be a const
+			dun_cell.position = Vector3(cell) #+ Vector3(0.5, 0, 0.5) #This offest should be a const
 			add_child(dun_cell)
 			dun_cell.set_owner(owner)
 			for i in 4: #Number of sides of a room
@@ -101,5 +211,6 @@ func create_dungeon():
 				else:
 					var key: String = str(cell_index) + str(cell_n_index)
 					call("handle_"+key, dun_cell, directions.keys()[i])
+			#handle_corners(dun_cell)
 				
 	
--- 3D_Procedural_Generation/3d-procedural-generation/MeshLibrary/dungeon_cell.gd
+++ 3D_Procedural_Generation/3d-procedural-generation/MeshLibrary/dungeon_cell.gd
@@ -1,30 +1,59 @@
 @tool
-extends Node3D
+class_name DungeonCell extends Node3D
 
+
+func show_wall_corner_ne():
+	$wall_corner_NE.visible= true
+func show_wall_corner_nw():
+	$wall_corner_NW.visible= true
+func show_wall_corner_sw():
+	$wall_corner_SW.visible= true
+func show_wall_corner_se():
+	$wall_corner_SE.visible= true
+func show_wall_corner_ne_inv():
+	$wall_corner_NE_Inv.visible= true
+func show_wall_corner_nw_inv():
+	$wall_corner_NW_Inv.visible= true
+func show_wall_corner_sw_inv():
+	$wall_corner_SW_Inv.visible= true
+func show_wall_corner_se_inv():
+	$wall_corner_SE_Inv.visible= true
 func remove_wall_corner_ne():
-	$wall_corner_NE.free()
+	$wall_corner_NE.visible = false
 func remove_wall_corner_nw():
-	$wall_corner_NW.free()
+	$wall_corner_NW.visible = false
 func remove_wall_corner_sw():
-	$wall_corner_SW.free()
+	$wall_corner_SW.visible = false
 func remove_wall_corner_se():
-	$wall_corner_SE.free()
+	$wall_corner_SE.visible = false
+func remove_wall_corner_ne_inv():
+	$wall_corner_NE_Inv.visible = false
+func remove_wall_corner_nw_inv():
+	$wall_corner_NW_Inv.visible = false
+func remove_wall_corner_sw_inv():
+	$wall_corner_SW_Inv.visible = false
+func remove_wall_corner_se_inv():
+	$wall_corner_SE_Inv.visible = false
 func remove_corners(corners_to_keep: Array[String]):
-	#TODO - handle logic to remove all corners not included
-	pass
+	var corner_list: Array[String] = ["ne", "nw", "se", "sw", "ne_inv", "nw_inv", "se_inv", "sw_inv"]
+	for c in corner_list:
+		if !corners_to_keep.has(c):
+			call("remove_wall_corner_"+c)
+		else:
+			call("show_wall_corner_"+c)
 func remove_wall_n():
-	$wall_N.free()
+	$wall_N.visible = false
 func remove_door_n():
-	$wall_doorway_N.free()
+	$wall_doorway_N.visible = false
 func remove_wall_s():
-	$wall_S.free()
+	$wall_S.visible = false
 func remove_door_s():
-	$wall_doorway_S.free()
+	$wall_doorway_S.visible = false
 func remove_wall_e():
-	$wall_E.free()
+	$wall_E.visible = false
 func remove_door_e():
-	$wall_doorway_E.free()
+	$wall_doorway_E.visible = false
 func remove_wall_w():
-	$wall_W.free()
+	$wall_W.visible = false
 func remove_door_w():
-	$wall_doorway_W.free()
+	$wall_doorway_W.visible = false
